6a9cdc4c2a5b285fd0fa7f9b96e1077f6eb37a64
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-10-16 14:30:20 +0530

    server changes

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use('/api/pizzas/', pizzasRoute);
 app.use('/api/users/', userRoute);
 app.use('/api/orders/', ordersRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use('/', express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 8000;
 
